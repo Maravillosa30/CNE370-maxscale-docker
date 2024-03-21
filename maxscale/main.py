4a9d06c8e88a20115d13a4b6e99ddaeb5a3d38b3
Maravillosa30 <132976690+Maravillosa30@users.noreply.github.com>
2024-03-20 21:39:41 -0700

    Update main.py

--- maxscale/main.py
+++ maxscale/main.py
@@ -44,17 +44,17 @@ for row in total_wages_pa:
     print(row[0])
     break
         
-# Zipcodes_second Queries: 
+# Zipcodes_two Queries: 
 
-# Query 1: The largest zipcode in zipcodes_second
-cursor.execute("SELECT * FROM zipcodes_second.zipcodes_second")
+# Query 1: The largest zipcode in zipcodes_two
+cursor.execute("SELECT * FROM zipcodes_two.zipcodes_two")
 largest_zipcode = cursor.fetchall()
 for zipcode in largest_zipcode:
-        print("The largest zipcode in zipcodes_second:", largest_zipcode)
+        print("The largest zipcode in zipcodes_two:", largest_zipcode)
         break
         
 # Query 2: All zipcodes where state=KY (Kentucky)
-cursor.execute("SELECT * FROM zipcodes_second.zipcodes_second  WHERE state='KY'")
+cursor.execute("SELECT * FROM zipcodes_two.zipcodes_two  WHERE state='KY'")
 ky_zipcodes = cursor.fetchall()
 print("All zipcodes where state=KY (Kentucky):")
 for row in ky_zipcodes:
@@ -62,7 +62,7 @@ for row in ky_zipcodes:
     break
 
 # Query 3: All zipcodes between 40000 and 41000
-cursor.execute("SELECT * FROM zipcodes_second.zipcodes_second WHERE zipcode BETWEEN 40000 AND 41000")
+cursor.execute("SELECT * FROM zipcodes_two.zipcodes_two WHERE zipcode BETWEEN 40000 AND 41000")
 zipcodes_between = cursor.fetchall()
 print("All zipcodes between 40000 and 41000:")
 for row in zipcodes_between:
@@ -70,7 +70,7 @@ for row in zipcodes_between:
     break
 
 # Query 4: The TotalWages column where state=PA (Pennsylvania)
-cursor.execute("SELECT TotalWages FROM zipcodes_second.zipcodes_second WHERE state='PA'")
+cursor.execute("SELECT TotalWages FROM zipcodes_two.zipcodes_two WHERE state='PA'")
 total_wages_pa = cursor.fetchall()
 print("The TotalWages column where state=PA (Pennsylvania):")
 for row in total_wages_pa:
